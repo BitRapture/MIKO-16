ddc6d2b5db8f3dfb94b7b3fc7df6084aceed4b39
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-07-09 03:32:54 +0100

    add window and renderer

--- MIKO-16/MIKO Main.cpp
+++ MIKO-16/MIKO Main.cpp
@@ -3,10 +3,29 @@
 
 int main(int, char**)
 {
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+	{
+		std::cout << "[SDL2] Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
-	SDL_Quit();
-	return 0;
+
+	SDL_Window* miko_window = SDL_CreateWindow("MIKO-16", 100, 100, 256, 240, SDL_WINDOW_SHOWN);
+	if (miko_window == nullptr)
+	{
+		std::cout << "[SDL2] Window Creation Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Renderer* miko_render = SDL_CreateRenderer(miko_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (miko_render == nullptr)
+	{
+		SDL_DestroyWindow(miko_window);
+		std::cout << "[SDL2] Renderer Creation Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+
+	return 1;
 }
