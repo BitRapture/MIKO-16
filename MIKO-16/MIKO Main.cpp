d9be3ad49187e5b56dc010ef819d66876a9d18df
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-07-12 03:39:50 +0100

    add fps cap and rework game loop

--- MIKO-16/MIKO Main.cpp
+++ MIKO-16/MIKO Main.cpp
@@ -16,7 +16,7 @@ int main(int, char**)
 		return 401;
 	}
 
-	SDL_Renderer* miko_render = SDL_CreateRenderer(miko_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Renderer* miko_render = SDL_CreateRenderer(miko_window, -1, SDL_RENDERER_ACCELERATED);
 	if (miko_render == nullptr)
 	{
 		cleanup(miko_window);
@@ -25,77 +25,37 @@ int main(int, char**)
 		return 402;
 	}
 
-	SDL_Event miko_event;
-	MIKO_System MIKO;	// Instanciate the system
+	MIKO_System MIKO;
+	if (MIKO.initialise(miko_render))
+	{
+		cleanup(miko_window, miko_render);
+		SDL_Log("[MIKO] Memory Allocation Error, failed to allocate memory");
+		SDL_Quit();
+		return 403;
+	}
+
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+	Uint32 frameStart;
+	int frameTime;
 
 	// Miko's game loop
 	while (MIKO.miko_sys_running)
 	{
-		while (SDL_PollEvent(&miko_event))
-		{
-			switch (miko_event.type)
-			{
-			case SDL_QUIT:
-				MIKO.miko_sys_running = false;
-				break;
-			case SDL_KEYDOWN:
-				switch (miko_event.key.keysym.sym)
-				{
-				case SDLK_RIGHT:
-					MIKO.miko_keys |= 0x1;
-					break;
-				case SDLK_LEFT:
-					MIKO.miko_keys |= 0x2;
-					break;
-				case SDLK_UP:
-					MIKO.miko_keys |= 0x4;
-					break;
-				case SDLK_DOWN:
-					MIKO.miko_keys |= 0x8;
-					break;
-				case SDLK_z:
-					MIKO.miko_keys |= 0x10;
-					break;
-				case SDLK_x:
-					MIKO.miko_keys |= 0x20;
-					break;
-				case SDLK_ESCAPE:
-					MIKO.miko_keys |= 0x40;
-					break;
-				}
-				break;
-			case SDL_KEYUP:
-				switch (miko_event.key.keysym.sym)
-				{
-				case SDLK_RIGHT:
-					MIKO.miko_keys ^= 0x1;
-					break;
-				case SDLK_LEFT:
-					MIKO.miko_keys ^= 0x2;
-					break;
-				case SDLK_UP:
-					MIKO.miko_keys ^= 0x4;
-					break;
-				case SDLK_DOWN:
-					MIKO.miko_keys ^= 0x8;
-					break;
-				case SDLK_z:
-					MIKO.miko_keys ^= 0x10;
-					break;
-				case SDLK_x:
-					MIKO.miko_keys ^= 0x20;
-					break;
-				case SDLK_ESCAPE:
-					MIKO.miko_keys ^= 0x40;
-					break;
-				}
-				break;
-			}
-		}
-
+		frameStart = SDL_GetTicks();
 		
-	}
+		SDL_SetRenderDrawColor(miko_render, 0, 0, 0, 0xff);
+		SDL_RenderClear(miko_render);
 
+		MIKO.updateKeys();
+		MIKO.updateSys();
+
+		SDL_RenderPresent(miko_render);
+
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime)
+			SDL_Delay(frameDelay - frameTime);
+	}
 
 	cleanup(miko_window, miko_render);
 	SDL_Quit();
