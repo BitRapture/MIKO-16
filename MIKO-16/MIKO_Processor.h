46a1488319c35f64f3c6587f99ca2d3e32f25a7c
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-07-12 03:39:30 +0100

    add memory reference and change stack type

--- MIKO-16/MIKO_Processor.h
+++ MIKO-16/MIKO_Processor.h
@@ -1,18 +1,20 @@
 #pragma once
 #include <SDL.h>
 
-#include "MIKO_System.h"
+#include "MIKO_Memory.h"
 
 class MIKO_Processor
 {
 private:
-	Uint8 reg_a = 0, reg_x = 0, reg_y = 0, reg_sp = 0, reg_status = 0;
+	Uint8 reg_a = 0, reg_x = 0, reg_y = 0, reg_sp = 0xff, reg_status = 0;
 	// Initialise the CPU registers
 
 	long long int reg_pc = 0;
 	// Initialise the CPU program counter (64 bit) a.k.a Instruction Pointer
 
 public:
+	MIKO_Memory* to_mem = nullptr;
+	// Lets the CPU access Memory
 
 };
 
